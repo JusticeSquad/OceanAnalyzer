9a9db512dadfec35fa4101606bc71b399aa73020
Justeson, Peter <pjusteson@gmail.com>
2020-08-15 13:16:17 -0400

    Added basic homepage and styling.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import Home from './feature/Home';
 
-class App extends React.PureComponent {
-	render() {
-		return (
-			<div>
-				<h1>Hello World!</h1>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div>
+			<Home>
+				<div>App Test</div>
+			</Home>
+		</div>
+	);
 }
 
 export default App;
